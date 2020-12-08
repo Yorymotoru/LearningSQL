43bb431779043fa6fb6876baeb233a6d7f876014
Yorymotoru <sl.borisov.42@gmail.com>
2020-12-08 17:29:23 +0400

    Practice 3 | 2 done

--- Practice 3/Task 2.sql
+++ Practice 3/Task 2.sql
@@ -1,2 +1,18 @@
 SELECT *
-FROM
+FROM CONTR c
+         LEFT JOIN PERS p
+                   ON c.PERS_ID = p.PERS_ID
+         JOIN (SELECT *
+                    FROM DEPART
+                    WHERE 1500 BETWEEN MIN_SALARY AND MAX_SALARY) d
+                   ON p.DEPART_ID = d.DEPART_ID
+WHERE c.DEPART_ID IS NOT NULL;
+SELECT *
+FROM CONTR c,
+     PERS p,
+     (SELECT *
+      FROM DEPART
+      WHERE 1500 BETWEEN MIN_SALARY AND MAX_SALARY) d
+WHERE c.PERS_ID(+) = p.PERS_ID
+  AND p.DEPART_ID(+) = d.DEPART_ID
+  AND c.DEPART_ID IS NOT NULL;
